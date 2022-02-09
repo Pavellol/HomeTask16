7c2d3a8ae8934dddef02406f6cfae575568d42dd
Pavellol <31768806+Pavellol@users.noreply.github.com>
2022-02-09 20:20:12 +0300

    Add files via upload

---
+++ HomeTask16/HomeTask16/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * 
+ * Написать метод, который выводит сумму элементов при помощи рекурсии
+ */
+using System;
+
+class Program
+{
+    static void SumAllElements(int[] Array, ref int i, ref int result)
+    {
+        
+        result += Array[i];
+        
+        i++;
+
+        if(i == Array.Length)
+        {
+            Console.WriteLine(result);
+            return;
+        }
+
+        SumAllElements(Array, ref i, ref result);
+    }
+
+    static void Main(string[] args)
+    {
+        int result = 0;
+        int i = 0;
+        int[] Array = new int[] { 1, 2, 3, 4, 5 };
+
+        SumAllElements(Array, ref i, ref result);
+    }
+}
